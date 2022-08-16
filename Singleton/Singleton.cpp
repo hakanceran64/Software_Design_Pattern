5f201091c7d74d6dfb21a914ff27ece627ba85fd
Hakan CERAN <hakanceran64@gmail.com>
2022-08-16 15:19:09 +0300

    Singleton

---
+++ Singleton/Singleton.cpp
@@ -0,0 +1,54 @@
+/*
+ * @Author:
+ * @Date:
+ * @Content: Singleton tasarým deseni:
+ *           bir sýnýfýn somutlaþtýrýlmasýný tek bir nesneyle sýnýrlamak için kullanýlan bir yazýlým tasarým ilkesidir.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+class Singleton {
+	private:
+		static Singleton *instance;
+		int data;
+		
+		// private constructor so that no object can be created
+		Singleton(){
+			data = 0;
+		}
+
+	public:
+		static Singleton *getInstance() {
+			if(!instance) {
+				instance = new Singleton;
+			}
+			
+			return instance;
+		}
+		
+		int getData() {
+			return this -> data;
+		}
+		
+		void setData(int data) {
+			this -> data = data;
+		}
+};
+
+// Initialize pointer to zero so that it can be initialized in first call to getInstance.
+Singleton *Singleton::instance = 0;
+
+int main(){
+	
+	Singleton *s = s -> getInstance();
+	
+	cout << s -> getData() << endl;
+	
+	s -> setData(100);
+	
+	cout << s -> getData() << endl;
+	
+	return 0;
+}
